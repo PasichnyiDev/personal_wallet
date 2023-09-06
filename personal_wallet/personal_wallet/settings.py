3425786d3a95202d28ea2790503db1b9bb599910
Anton <pasichny.law@gmail.com>
2023-09-06 10:46:01 +0300

    prepare to deploy

--- personal_wallet/personal_wallet/settings.py
+++ personal_wallet/personal_wallet/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -36,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'users',
     'wallets',
     'rest_framework',
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -140,4 +142,3 @@ SIMPLE_JWT = {
 
 # custom user
 AUTH_USER_MODEL = 'users.WalletUser'
-
