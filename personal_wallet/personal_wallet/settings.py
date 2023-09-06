08150c40ea94f01f8ea571acf1a026dcb6b9f279
Anton <pasichny.law@gmail.com>
2023-09-06 10:24:24 +0300

    prepare to deploy

--- personal_wallet/personal_wallet/settings.py
+++ personal_wallet/personal_wallet/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-omppo1qoxq^h%k=5a*te8n3n2#8e+z_b04t5vn5_d7fr4wsb@d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
